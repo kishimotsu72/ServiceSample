561ad20f43e131fd731a4ae83b7217f067a26e8d
kishimotsu72 <39108433+kishimotsu72@users.noreply.github.com>
2018-07-05 10:42:20 +0900

    Update SoundStartActivity.java

--- app/src/main/java/com/websarva/wings/android/servicesample/SoundStartActivity.java
+++ app/src/main/java/com/websarva/wings/android/servicesample/SoundStartActivity.java
@@ -14,11 +14,10 @@ public class SoundStartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sound_start);
 
         Intent intent = getIntent();
-        //通知のタップからの引き継ぎデータを取得。
         boolean fromNotification = intent.getBooleanExtra("fromNotification", false);
-        //引き継ぎデータが存在、つまり通知のタップからならば…
+
         if(fromNotification) {
-            //再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
+    
             Button btPlay = findViewById(R.id.btPlay);
             Button btStop = findViewById(R.id.btStop);
             btPlay.setEnabled(false);
@@ -26,34 +25,21 @@ public class SoundStartActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * 再生ボタンタップ時の処理メソッド。
-     *
-     * @param view 画面部品
-     */
     public void onPlayButtonClick(View view) {
-        //インテントオブジェクトを生成。
         Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
-        //サービスを起動。
         startService(intent);
-        //再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
         Button btPlay = findViewById(R.id.btPlay);
         Button btStop = findViewById(R.id.btStop);
         btPlay.setEnabled(false);
         btStop.setEnabled(true);
     }
 
-    /**
-     * 停止ボタンタップ時の処理メソッド。
-     *
-     * @param view 画面部品
-     */
     public void onStopButtonClick(View view) {
-        //インテントオブジェクトを生成。
+
         Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
-        //サービスを停止。
+
         stopService(intent);
-        //再生ボタンをタップ可に、停止ボタンをタップ不可に変更。
+
         Button btPlay = findViewById(R.id.btPlay);
         Button btStop = findViewById(R.id.btStop);
         btPlay.setEnabled(true);
